60dbe344254f2af6e6da2fa5700ca198440e4e0d
VitalyMikulich <mr.wital1999@gmail.com>
2019-11-23 21:30:57 +0300

    Updates

---
+++ precache-manifest.9ff5f83037b55526a95dc8f85026ded0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb59bae79b802327147d9d6e48231839",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "4b02b268c8ad7fa14c66",
+    "url": "/tic-tac-toe-react/static/css/main.f0225d84.chunk.css"
+  },
+  {
+    "revision": "cc2330b4031aac99cd58",
+    "url": "/tic-tac-toe-react/static/js/2.4227058b.chunk.js"
+  },
+  {
+    "revision": "4b02b268c8ad7fa14c66",
+    "url": "/tic-tac-toe-react/static/js/main.b8e68e89.chunk.js"
+  },
+  {
+    "revision": "c650f4249a1a9ac15f21",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.27a0c5ca.js"
+  }
+]);
